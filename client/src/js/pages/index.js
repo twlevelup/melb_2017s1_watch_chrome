acc3d30d305f38ef3d2199451f67bf059653eb91
Jonathon Grigg <jonathongrigg@gmail.com>
2017-04-11 20:26:40 +1000

    (Afzal and Jonathon) Functionality for changing the displayed question and answers

--- client/src/js/pages/index.js
+++ client/src/js/pages/index.js
@@ -4,6 +4,7 @@ const home = require('./homePage');
 const contacts = require('./contactsPage');
 const eventsList = require('./eventsList');
 const eventDetails = require('./eventDetails');
+const snapGame = require('./snapGamePage');
 
 // TODO autogenerate this file
 module.exports = {
@@ -13,4 +14,5 @@ module.exports = {
   contacts,
   eventsList,
   eventDetails,
+  snapGame,
 };
